64c4f6e7c5a7b8f34cf46fec1ffbbb17bde34810
Pratyush Choudhary <solixious@gmail.com>
2026-07-04 19:02:27 +0530

    Fixing the default redirect page for non logged in user.

--- src/main/java/in/bulletbeats/config/SecurityConfig.java
+++ src/main/java/in/bulletbeats/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
@@ -130,6 +131,7 @@ public class SecurityConfig {
             )
             .exceptionHandling(ex -> ex
                 .accessDeniedPage("/403")
+                .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/public/menu"))
             )
             .addFilterBefore(rateLimitFilter, UsernamePasswordAuthenticationFilter.class)
             .addFilterBefore(setupCheckFilter(), UsernamePasswordAuthenticationFilter.class);
